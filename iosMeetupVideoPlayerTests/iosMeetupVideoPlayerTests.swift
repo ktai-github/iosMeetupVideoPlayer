c76a73cd03459d763eae62111ffa8a9417d5fa54
KevinT <kevin.tai.mail@gmail.com>
2018-01-06 10:10:18 -0800

    Initial Commit

---
+++ iosMeetupVideoPlayerTests/iosMeetupVideoPlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  iosMeetupVideoPlayerTests.swift
+//  iosMeetupVideoPlayerTests
+//
+//  Created by KevinT on 2018-01-06.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+import XCTest
+@testable import iosMeetupVideoPlayer
+
+class iosMeetupVideoPlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
