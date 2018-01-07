a8ba1657f4703d17bcf52f46148b9bedb43f747a
KevinT <kevin.tai.mail@gmail.com>
2018-01-07 08:23:28 -0800

    first commit

--- iosMeetupVideoPlayer/ViewController.swift
+++ iosMeetupVideoPlayer/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet var playerView: UIView!
+  var player: AVPlayer!
+  var playerLayer: AVPlayerLayer!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    initPlayer()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func initPlayer() {
+    let videoUrl = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+    player = AVPlayer(url: videoUrl!)
+    playerLayer = AVPlayerLayer(player: player)
+    playerView.layer.addSublayer(playerLayer)
   }
-
-
+  
+  override func viewDidLayoutSubviews() {
+    playerLayer.frame = playerView.bounds
+  }
+  
+  @IBAction func play(_ sender: Any) {
+    player.play()
+  }
+  
+  @IBAction func stop(_ sender: Any) {
+    player.pause()
+  }
+  
+  @IBAction func retry(_ sender: Any) {
+    player.seek(to: kCMTimeZero)
+  }
 }
-
